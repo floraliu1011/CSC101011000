75d51fb88a1778b4914d9b58b764a225f8376771
Flora Liu <yufang.liu@mail.utoronto.ca>
2018-04-25 12:01:01 -0400

    debugged the setup.sh

--- setup.sh
+++ setup.sh
@@ -30,8 +30,8 @@ then
 fi
 
 # generate file name
-forward_name=`cut -d "." -f1 "${forward_path}"`
-reverse_name=`cut -d "." -f1 "${reverse_path}"`
+forward_name=`echo ${forward_path} | cut -d "." -f1` 
+reverse_name=`echo ${reverse_path} | cut -d "." -f1 `
 
 # prepare the directories
 mkdir -p Data Intermediate Output/Logfile
@@ -47,27 +47,27 @@ else
 fi
 
 # split the files
-split -l${line} -d "Data/${forward_name}*" "${forward_name}"
-split -l${line} -d "Data/${reverse_name}*" "${reverse_name}"
+split -l${line} -d Data/${forward_name}* "${forward_name}"
+split -l${line} -d Data/${reverse_name}* "${reverse_name}"
 mv ${forward_name}* Intermediate
 mv ${reverse_name}* Intermediate
 
 # make the submission file
 cat > submission <<EOF
 #!/bin/bash
-#PBS -l nodes=$(( $NUM_FILE / 16 + 1)):ppn=8,walltime=6:00:00
+#PBS -l nodes=1:ppn=8,walltime=6:00:00
 #PBS -o output.out
 #BPS -e output.err
 
 module load intel/15.0.2
 module load python/3.5.1
 
-cd $PBS_O_WORKDIR
+cd \$PBS_O_WORKDIR
 EOF
 ((max=$num_split - 1))
 for i in `seq -w 00 $max`
 do
-	echo "sh Script/Parser/parse.sh Intermediate/${forward_name}$i Intermediate/${reverse_name}$i Output/LogFile/log_file${i}.txt >> Intermediate/parsed_sequences.txt" >> submission
+	echo "(sh Script/Parser/parse.sh Intermediate/${forward_name}$i Intermediate/${reverse_name}$i Output/LogFile/log_file${i}.txt >> Intermediate/parsed_sequences.txt")& >> submission
 done
 	echo 'wait&' >> submission
 
